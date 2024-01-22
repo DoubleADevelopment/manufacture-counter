d0e2c31db160cd1a6de0c6428af950205ddda0b5
Anton Ashurek <antonashurek@192.168.1.1>
2024-01-22 14:17:44 +0100

    create base routing template and main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+//pages
+import * as Pages from './pages';
+
+const App = (): JSX.Element => {
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path={'/'} element={<Pages.MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/app/pages/main-page/main-page.tsx
@@ -0,0 +1,8 @@
+//styles
+import './main-page.scss';
+
+const MainPage = (): JSX.Element => {
+  return <div className="main-page">main</div>;
+};
+
+export default MainPage;
