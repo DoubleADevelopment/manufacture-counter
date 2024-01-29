5beda45f7c021b683631fa67d432b61bce3f646e
Anton Ashurek <antonashurek@192.168.1.1>
2024-01-29 17:56:06 +0100

    add fonts

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,11 +3,21 @@ import { ReactNode } from 'react';
 import './styles/container.scss';
 import './styles/global-styles.scss';
 import './styles/semantic.scss';
+import './styles/fonts.scss';
 import './styles/variables/core.scss';
 import './styles/variables/primitives.scss';
 import './styles/variables/semantic.scss';
 import './styles/variables/variables.scss';
 import './styles/variables/width-variables.scss';
+//fonts
+import './fonts/UberMoveTextLight.woff';
+import './fonts/UberMoveTextLight.woff2';
+import './fonts/UberMoveTextMedium.woff';
+import './fonts/UberMoveTextMedium.woff2';
+import './fonts/UberMoveTextRegular.woff';
+import './fonts/UberMoveTextRegular.woff2';
+import './fonts/UberMoveTextBold.woff';
+import './fonts/UberMoveTextBold.woff2';
 
 type LayoutProps = {
   children: ReactNode;
