5d0b1924ab5e65152e3577e792d1cb10309b7573
Anton Ashurek <antonashurek@192.168.1.1>
2024-01-29 17:28:14 +0100

    create router component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,12 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-//pages
-import * as Pages from '../pages';
-//variables
-import { RoutingPaths, rootBaseName } from '../variables/routing-variables';
+//router
+import Router from './router/router';
 //styles
 import './app.scss';
 
 const App = (): JSX.Element => {
   return (
     <div className="app">
-      <BrowserRouter basename={rootBaseName}>
-        <Routes>
-          <Route path={RoutingPaths.ROOT} element={<Pages.MainPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Router />
     </div>
   );
 };
