92fa5201a8c38520f8d1c9f8f5e347e544fd3760
Anton Ashurek <antonashurek@192.168.1.1>
2024-01-29 17:20:11 +0100

    create layout for index and import all styles to layout

---
+++ src/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+//all necessary styles
+import './styles/container.scss';
+import './styles/global-styles.scss';
+import './styles/variables/core.scss';
+import './styles/variables/primitives.scss';
+import './styles/variables/semantic.scss';
+import './styles/variables/variables.scss';
+import './styles/variables/width-variables.scss';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return <>{children}</>;
+};
+
+export default Layout;
