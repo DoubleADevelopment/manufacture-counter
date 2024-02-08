0cbe35595350f5dfadc0fa20af37614ed44f53be
Anton Ashurek <antonashurek@192.168.1.1>
2024-02-08 14:02:39 +0100

    add counter list data

--- src/components/counters-list/counters-list.tsx
+++ src/components/counters-list/counters-list.tsx
@@ -1,36 +1,16 @@
+//components
+import CountersListItem from './counter-list-item/counter-list-item';
+//data
+import { countersListData } from '../../data/counters-list-data';
 //styles
 import style from './counters-list.module.scss';
 
 const CountersList = (): JSX.Element => {
   return (
     <ul className={style['counters-list']}>
-      <li className={style['counter-list__item']}>
-        <h3 className={`${style['counter-list__title']} heading-medium`}>Pigmenty</h3>
-        <img
-          className={style['counters-list__image']}
-          src="https://webp-konwerter.infor.pl/eyJmIjoiaHR0cHM6Ly9nO/S5pbmZvci5wbC9wL19maW/xlcy8yMDIwMDAvcGlnbWV/udHkuanBnIiwidyI6ODAwfQ.png"
-          alt=""
-        />
-        <a className={style['counters-list__link']} href=""></a>
-      </li>
-      <li className={style['counter-list__item']}>
-        <h3 className={`${style['counter-list__title']} heading-medium`}>Uszczelka</h3>
-        <img
-          className={style['counters-list__image']}
-          src="https://media.istockphoto.com/id/1221227912/pl/zdj%C4%99cie/granulki-z-czarnej-gumy.jpg?s=612x612&w=is&k=20&c=BDCbsa8HfJpGgPErJtdesdVwAA7vJCu_kZchYSGVaIE="
-          alt=""
-        />
-        <a className={style['counters-list__link']} href=""></a>
-      </li>
-      <li className={style['counter-list__item']}>
-        <h3 className={`${style['counter-list__title']} heading-medium`}>Chemia</h3>
-        <img
-          className={style['counters-list__image']}
-          src="https://prus.edu.pl/wp-content/uploads/2022/09/chemia-.jpg"
-          alt=""
-        />
-        <a className={style['counters-list__link']} href=""></a>
-      </li>
+      {countersListData.map((item) => {
+        return <CountersListItem item={item} />;
+      })}
     </ul>
   );
 };
---
+++ src/data/counters-list-data.ts
@@ -0,0 +1,24 @@
+//types
+import { CountersListDataType } from '../types';
+
+const countersListData: CountersListDataType = [
+  {
+    title: 'Pigmenty',
+    link: './',
+    image:
+      'https://webp-konwerter.infor.pl/eyJmIjoiaHR0cHM6Ly9nO/S5pbmZvci5wbC9wL19maW/xlcy8yMDIwMDAvcGlnbWV/udHkuanBnIiwidyI6ODAwfQ.png',
+  },
+  {
+    title: 'Uszczelka',
+    link: './',
+    image:
+      'https://media.istockphoto.com/id/1221227912/pl/zdj%C4%99cie/granulki-z-czarnej-gumy.jpg?s=612x612&w=is&k=20&c=BDCbsa8HfJpGgPErJtdesdVwAA7vJCu_kZchYSGVaIE=',
+  },
+  {
+    title: 'Chemia',
+    link: '',
+    image: 'https://prus.edu.pl/wp-content/uploads/2022/09/chemia-.jpg',
+  },
+];
+
+export { countersListData };
