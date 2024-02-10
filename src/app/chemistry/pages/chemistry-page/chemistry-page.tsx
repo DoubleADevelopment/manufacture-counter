d5277ab0af909b2972ba1da46d94947fb7ea431d
Anton Ashurek <antonashurek@192.168.1.1>
2024-02-10 19:35:10 +0100

    create union types for components data

--- src/app/chemistry/pages/chemistry-page/chemistry-page.tsx
+++ src/app/chemistry/pages/chemistry-page/chemistry-page.tsx
@@ -1,15 +1,34 @@
+//store
+import { useAppSelector } from '../../../../hooks/hooks';
+import { SelectorGetChemistryState } from '../../store/eslectors/selectors';
 //components
 import { Header, ComponentsList } from '../../../../components';
+//types
+import type { ChemistryDataForViewType, IChemistryDataType } from '../../types/data-types';
 //styles
 import style from './chemistry-page.module.scss';
 
 const ChemistryPage = () => {
+  const chemistryData = useAppSelector(SelectorGetChemistryState);
+
+  const storeDataToClient = (data: IChemistryDataType): ChemistryDataForViewType => {
+    const itemsArray: ChemistryDataForViewType = [];
+
+    for (const item in data) {
+      itemsArray.push(data[item]);
+    }
+
+    return itemsArray;
+  };
+
+  const adaptedData = storeDataToClient(chemistryData);
+
   return (
     <div className={`${style['chemistry-page']} container-mobile page`}>
       <Header />
       <main className={style['chemistry-page__content']}>
         <h2 className={`${style['chemistry-page__title']} heading-xl-large`}>Chemia</h2>
-        <ComponentsList />
+        <ComponentsList data={adaptedData} />
       </main>
     </div>
   );
--- src/components/components-list/components-list.tsx
+++ src/components/components-list/components-list.tsx
@@ -1,8 +1,24 @@
+//types
+import type { UnionComponentsDataForView } from '../../types/data-types';
 //styles
 import style from './components-list.module.scss';
 
-const ComponentsList = (): JSX.Element => {
-  return <ul className={style['components-list']}></ul>;
+interface IComponentsListProps {
+  data: UnionComponentsDataForView;
+}
+
+const ComponentsList = ({ data }: IComponentsListProps): JSX.Element => {
+  return (
+    <ul className={style['components-list']}>
+      {data.map((item) => {
+        return (
+          <li key={item.UNID}>
+            <h3>{item.name}</h3>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ComponentsList;
