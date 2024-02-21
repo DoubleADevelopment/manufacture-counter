f097a070b74d52d0131ec08dc0c44a3ca73a026c
Anton Ashurek <antonashurek@192.168.1.1>
2024-02-21 16:16:08 +0100

    create function fro choise components to render in counter

--- src/app/chemistry/components/counter/counter.tsx
+++ src/app/chemistry/components/counter/counter.tsx
@@ -13,21 +13,22 @@ import style from './counter.module.scss';
 const Counter = (): JSX.Element => {
   const { UNID } = useParams();
   const itemFromUnidIsset = useAppSelector(SelectorCheckIfElementExistsByUNID(UNID));
-  const displayCounter = UNID && itemFromUnidIsset;
 
-  return (
-    <main className={style['counter']}>
-      {displayCounter ? (
-        <CounterControls UNID={UNID} />
-      ) : (
+  const renderComponents = (): JSX.Element => {
+    if (UNID && itemFromUnidIsset) {
+      return <CounterControls UNID={UNID} />;
+    } else {
+      return (
         <PageNotification
           headingText="Błąd przy otwarciu licznika"
           paragraphText={ErrorsText.COMPONENT_NOT_FOUND}
           type={NotificationType.ERROR}
         />
-      )}
-    </main>
-  );
+      );
+    }
+  };
+
+  return <main className={style['counter']}>{renderComponents()}</main>;
 };
 
 export default Counter;
