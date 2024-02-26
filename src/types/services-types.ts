48e255a7c04c43d6f13b75bc34a904ad029710de
Anton Ashurek <antonashurek@192.168.1.1>
2024-02-26 22:40:16 +0100

    foo

--- src/types/services-types.ts
+++ src/types/services-types.ts
@@ -18,14 +18,4 @@ interface IAdapterService<D, P> {
   adaptDataToApp(dataPackage: P): D;
 }
 
-abstract class ADS<D extends object> {
-  #data: D;
-  constructor(data: D) {
-    this.#data = data;
-  }
-  getDataItem<P extends keyof D>(id: keyof D[P], packageName: P) {
-    return this.#data[packageName][id];
-  }
-}
-
-export type { IDataService, ILocalstorageService, IAdapterService, ADS };
+export type { IDataService, ILocalstorageService, IAdapterService };
