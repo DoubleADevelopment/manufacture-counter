1ad5fad7b9aad4779079ebf1e7e680b898f24659
Anton Ashurek <antonashurek@192.168.1.1>
2024-02-26 17:27:04 +0100

    add abstract localstorage class for all apps

--- src/app/gums/services/gums-localstorage.service.ts
+++ src/app/gums/services/gums-localstorage.service.ts
@@ -1,53 +1,17 @@
 //types
-import type { LocalstorageNamesType, ILocalstorageService } from '../../../types';
+import type { LocalstorageNamesType } from '../../../types';
 import type { IGumsDataType } from '../types/data-types';
 //variables
 import { GUMS_STORAGE_NAME } from '../variables/data-variables';
+//abstract
+import { AbstractLocalstorageService } from '../../../services';
 
-class ChemistryLocalstorageService implements ILocalstorageService<IGumsDataType> {
-  #storageName: LocalstorageNamesType;
-
+class GumsLocalstorageService extends AbstractLocalstorageService<IGumsDataType> {
   constructor(storageName: LocalstorageNamesType) {
-    this.#storageName = storageName;
-  }
-
-  getItems(): IGumsDataType | Error | null {
-    try {
-      const jsonData: string | null = localStorage.getItem(this.#storageName);
-
-      if (jsonData) {
-        const parsedJsonData: IGumsDataType = JSON.parse(jsonData);
-        return parsedJsonData;
-      } else {
-        return null;
-      }
-    } catch (err) {
-      if (err instanceof Error) {
-        return new Error(err.message);
-      } else {
-        return new Error('Failed to get data from localStorage');
-      }
-    }
-  }
-
-  setItems(data: IGumsDataType): IGumsDataType | Error {
-    try {
-      localStorage.setItem(this.#storageName, JSON.stringify(data));
-      return data;
-    } catch (err) {
-      if (err instanceof Error) {
-        return new Error(err.message);
-      } else {
-        return new Error('Failed to write data to localStorage');
-      }
-    }
-  }
-
-  clearStore() {
-    localStorage.removeItem(this.#storageName);
+    super(storageName);
   }
 }
 
-const chemistryLocalstorageService = new ChemistryLocalstorageService(GUMS_STORAGE_NAME);
+const gumsLocalstorageService = new GumsLocalstorageService(GUMS_STORAGE_NAME);
 
-export default chemistryLocalstorageService;
+export default gumsLocalstorageService;
--- src/app/pigments/services/pigments-localstorage.service.ts
+++ src/app/pigments/services/pigments-localstorage.service.ts
@@ -1,53 +1,16 @@
 //types
-import type { LocalstorageNamesType, ILocalstorageService } from '../../../types';
+import type { LocalstorageNamesType } from '../../../types';
 import type { IPigmentsDataType } from '../types/data-type';
 //variables
 import { PIGMENTS_STORAGE_NAME } from '../variables/data-variables';
+//abstract
+import { AbstractLocalstorageService } from '../../../services';
 
-class PigmentsLocalstorageService implements ILocalstorageService<IPigmentsDataType> {
-  #storageName: LocalstorageNamesType;
-
+class PigmentsLocalstorageService extends AbstractLocalstorageService<IPigmentsDataType> {
   constructor(storageName: LocalstorageNamesType) {
-    this.#storageName = storageName;
-  }
-
-  getItems(): IPigmentsDataType | Error | null {
-    try {
-      const jsonData: string | null = localStorage.getItem(this.#storageName);
-
-      if (jsonData) {
-        const parsedJsonData: IPigmentsDataType = JSON.parse(jsonData);
-        return parsedJsonData;
-      } else {
-        return null;
-      }
-    } catch (err) {
-      if (err instanceof Error) {
-        return new Error(err.message);
-      } else {
-        return new Error('Failed to get data from localStorage');
-      }
-    }
-  }
-
-  setItems(data: IPigmentsDataType): IPigmentsDataType | Error {
-    try {
-      localStorage.setItem(this.#storageName, JSON.stringify(data));
-      return data;
-    } catch (err) {
-      if (err instanceof Error) {
-        return new Error(err.message);
-      } else {
-        return new Error('Failed to write data to localStorage');
-      }
-    }
-  }
-
-  clearStore() {
-    localStorage.removeItem(this.#storageName);
+    super(storageName);
   }
 }
 
 const pigmentsLocalstorageService = new PigmentsLocalstorageService(PIGMENTS_STORAGE_NAME);
-
 export default pigmentsLocalstorageService;
