c04baf37734b8c39ef8f489b0f9420629e31974e
Anton Oshurek <oshurekweb@gmail.com>
2024-03-01 16:32:33 +0100

    create confirm button and modal for delete button

--- src/components/buttons/button-delete-with-confirm/button-delete-with-confirm.tsx
+++ src/components/buttons/button-delete-with-confirm/button-delete-with-confirm.tsx
@@ -1,17 +1,57 @@
 //types
+import { useState } from 'react';
 import type { IButtonProps } from '../../../types/';
 //styles
 import style from './button-delete-with-confirm.module.scss';
 
 const ButtonDeleteWithConfirm = ({ text, clickHandler }: IButtonProps): JSX.Element => {
+  const [confirmModalIsOpen, setConfirmModalIsOpen] = useState<boolean>(false);
+
+  const onDeleteButtonClickHandler = () => {
+    setConfirmModalIsOpen(true);
+  };
+
+  const confirmButtonClickHandler = () => {
+    clickHandler();
+    console.log('confirm');
+  };
+
+  const onCancelButtonClickHandler = () => {
+    setConfirmModalIsOpen(false);
+  };
+
   return (
-    <button
-      type="button"
-      className={`${style['button-delete-with-confirm']} label-small`}
-      onClick={clickHandler}
-    >
-      {text}
-    </button>
+    <>
+      <button
+        type="button"
+        className={`${style['button-delete-with-confirm']} label-small`}
+        onClick={onDeleteButtonClickHandler}
+      >
+        {text}
+      </button>
+
+      {confirmModalIsOpen && (
+        <div className={style['button-delete-with-confirm__modal']}>
+          <div className={style['button-delete-with-confirm__modal-content']}>
+            <h4>Clear your chat history - are you sure?</h4>
+            <button
+              onClick={onCancelButtonClickHandler}
+              className={style['button-delete-with-confirm__cancel-btn']}
+              type="button"
+            >
+              anulować
+            </button>
+            <button
+              onClick={confirmButtonClickHandler}
+              type="button"
+              className={`${style['button-delete-with-confirm']} label-small`}
+            >
+              Potwierdź usunięcie
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
