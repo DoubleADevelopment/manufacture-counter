b1cfbd65b81e41f98d36cb5846cb32f99353733c
Anton Oshurek <oshurekweb@gmail.com>
2024-03-06 20:07:55 +0100

    update gums data

---
+++ src/app/gums/types/index.ts
@@ -0,0 +1,9 @@
+import type {
+  IGumsDataItemType,
+  IGumsData,
+  IGumsDataPackageType,
+  IGumsLogs,
+  GumsItemsListType,
+} from './data-types';
+
+export type { IGumsDataItemType, IGumsData, IGumsDataPackageType, IGumsLogs, GumsItemsListType };
