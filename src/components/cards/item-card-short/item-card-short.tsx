ce3204ab6d2df40d3bbb4be4e5b989c1fa8cf727
Anton Oshurek <oshurekweb@gmail.com>
2024-03-06 18:51:57 +0100

    delete && in item card short

--- src/components/cards/item-card-short/item-card-short.tsx
+++ src/components/cards/item-card-short/item-card-short.tsx
@@ -28,25 +28,24 @@ const ItemCardShort = ({
           className={`${style['item-card-short']} ${withModal && style['item-card-short--with-modal']}`}
         >
           <div className={style['item-card-short__wrap']}>
-            <h3 className={`content-primary-a heading-small`}>{item && item.name}</h3>
+            <h3 className={`content-primary-a heading-small`}>{item.name}</h3>
             <p className={`content-primary-a paragraph-small`}>
-              <span className={`paragraph-secondary-a`}>Numer:</span> {item && item.itemNumber}
+              <span className={`paragraph-secondary-a`}>Numer:</span> {item.itemNumber}
             </p>
             <p className={`content-primary-a paragraph-small`}>
-              <span className={`content-secondary-a`}>Opakowanie:</span>{' '}
-              {item && item.packagingInfo}
+              <span className={`content-secondary-a`}>Opakowanie:</span> {item.packagingInfo}
             </p>
           </div>
           <img
             className={style['item-card-short__image']}
-            src={item && item.image}
-            alt={item && item.description}
+            src={item.image}
+            alt={item.description}
             width={150}
             height={150}
           />
           <div className={`${style['item-card-short__amount']} paragraph-primary-a label-medium`}>
             {measurementText}
-            <span className="paragraph-primary-a heading-medium">{item && item.amount}</span>
+            <span className="paragraph-primary-a heading-medium">{item.amount}</span>
           </div>
         </article>
       ) : (
