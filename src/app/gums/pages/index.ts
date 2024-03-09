e90ed6024259ba7b4551eddbcaf42060687c78d1
Anton Oshurek <oshurekweb@gmail.com>
2024-03-09 14:51:27 +0100

    fix gums router bug

--- src/app/gums/pages/index.ts
+++ src/app/gums/pages/index.ts
@@ -1,4 +1,4 @@
-import GumsRootPage from './gums-root-page/gums-root-page';
 import CounterPage from './counter-page/counter-page';
+import GumsRootPage from './gums-root-page/gums-root-page';
 
 export { GumsRootPage, CounterPage };
