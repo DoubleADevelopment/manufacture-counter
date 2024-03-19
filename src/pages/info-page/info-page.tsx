2ca6d6b0e0d519e15ec167395e0fb674d359c6c6
Anton Oshurek <oshurekdev@proton.me>
2024-03-19 15:19:19 +0100

    styling info page

--- src/pages/info-page/info-page.tsx
+++ src/pages/info-page/info-page.tsx
@@ -11,14 +11,14 @@ const InfoPage = (): JSX.Element => {
     <PageWithMenuLayout headerTitle={AppRouting.INFORMATION.title}>
       <main className={style['info-page']}>
         <section className={style.info}>
-          <h2 className={style.info__title}>Info about application</h2>
-          <p className={style.info__text}>
+          <h2 className={`content-primary-a heading-large`}>Info about application</h2>
+          <p className={`content-primary-a paragraph-medium`}>
             Hi everyone! My name is Anton and this counter was specially made for the Aluplast
             company in Poznań!
           </p>
           <div className={style.info__stack}>
-            <h3 className={style.info__subtitle}>technologies stack:</h3>
-            <ul className={(style.info__text, style['info__stack-list'])}>
+            <h3 className={`content-primary-a heading-medium`}>technologies stack:</h3>
+            <ul className={`${style['info__stack-list']} content-primary-a label-large`}>
               <li>
                 <code>javaScript</code>
               </li>
@@ -36,24 +36,26 @@ const InfoPage = (): JSX.Element => {
               </li>
             </ul>
           </div>
-          <p className={style.info__text}>
+          <p className={`content-primary-a paragraph-medium`}>
             Check all my projects in my GitHub profile :) and subscribe in another's social media.
           </p>
 
           <div className={style.info__socials}>
             <a
-              className={style['info__social-link']}
+              className={`${style['info__social-link']} content-primary-a`}
               href="https://github.com/AntonOshurek"
               target="blank"
             >
-              <GitHubIcon color="#000" />
+              <span className="visually-hidden">link na mój GitHub</span>
+              <GitHubIcon />
             </a>
             <a
-              className={style['info__social-link']}
+              className={`${style['info__social-link']} content-primary-a`}
               href="https://www.instagram.com/ooshurek/"
               target="blank"
             >
-              <InstagramIcon color="#000" />
+              <span className="visually-hidden">Link na mój instagram</span>
+              <InstagramIcon />
             </a>
           </div>
         </section>
