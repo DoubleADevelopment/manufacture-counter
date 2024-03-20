477f456de63f8f432682ed5b3de661a867d88653
Anton Oshurek <oshurekdev@proton.me>
2024-03-20 17:38:24 +0100

    commented unused routes in chemistry

--- src/pages/components-overview-page/components-overview-page.tsx
+++ src/pages/components-overview-page/components-overview-page.tsx
@@ -1,11 +1,17 @@
 //layouts
 import { PageWithMenuLayout } from '../../layouts';
+//store
+import { useAppSelector } from '../../hooks/hooks';
 //variables
 import { AppRouting } from '../../variables';
 //styles
 import style from './components-overview-page.module.scss';
+import { SelectorGetExtrusionChemistry } from '../../app/chemistry/store/slectors/selectors';
 
 const ComponentOverviewPage = (): JSX.Element => {
+  const chemistryData = useAppSelector(SelectorGetExtrusionChemistry());
+  console.log(chemistryData);
+
   return (
     <PageWithMenuLayout headerTitle={AppRouting.COMPONENTS_OVERVIEW.title}>
       <main className={style['components-overview-page']}></main>
