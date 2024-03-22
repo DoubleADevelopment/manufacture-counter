8f4106fb71a36c5c3de07689f222444f6aaaa21c
Anton Oshurek <oshurekdev@proton.me>
2024-03-22 22:46:22 +0100

    show test supplies data

--- src/app/supplies/pages/supplies-root-page/supplies-root-page.tsx
+++ src/app/supplies/pages/supplies-root-page/supplies-root-page.tsx
@@ -1,10 +1,10 @@
 //components
-import { PageNotification } from '../../../../components';
-
+import { ItemsList, PageNotification } from '../../../../components';
 //services
-
+import suppliesAdapterService from '../../services/supplies-adapter.service';
 //store
-
+import { SelectorGetSupplies } from '../../store/slectors/selectors';
+import { useAppSelector } from '../../../../hooks/hooks';
 //layouts
 import { PageWithMenuLayout } from '../../../../layouts';
 //variables
@@ -14,14 +14,21 @@ import { AppRouting, ErrorsText, NotificationType } from '../../../../variables'
 import style from './supplies-root-page.module.scss';
 
 const SuppliesRootPage = () => {
+  const suppliesState = useAppSelector(SelectorGetSupplies());
+  const adaptedData = suppliesAdapterService.adaptItemsDataToDisplayList(suppliesState);
+
   return (
     <PageWithMenuLayout headerTitle={SUPPLIES_TEXT.SUPPLIES} backLink={AppRouting.ROOT.path}>
       <main className={style['gums-root-page']}>
-        <PageNotification
-          type={NotificationType.ERROR}
-          headingText={ErrorsText.SOMETHING_WENT_WRONG}
-          paragraphText={ErrorsText.ITEMS_LIST_NOT_FOUND}
-        />
+        {adaptedData ? (
+          <ItemsList data={adaptedData} />
+        ) : (
+          <PageNotification
+            type={NotificationType.ERROR}
+            headingText={ErrorsText.SOMETHING_WENT_WRONG}
+            paragraphText={ErrorsText.ITEMS_LIST_NOT_FOUND}
+          />
+        )}
       </main>
     </PageWithMenuLayout>
   );
