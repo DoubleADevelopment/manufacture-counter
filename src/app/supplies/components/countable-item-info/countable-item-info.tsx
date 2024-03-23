2aa367d075c8c06fccac2d1e5755a31c09c82aa4
Anton Oshurek <oshurekdev@proton.me>
2024-03-23 14:10:38 +0100

    add countable item info

---
+++ src/app/supplies/components/countable-item-info/countable-item-info.tsx
@@ -0,0 +1,43 @@
+import { useParams } from 'react-router-dom';
+//components
+import { ShortCardWithLogsModal } from '../../../../components';
+//store
+import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
+import { clearItemDataAction } from '../../store/actions/actions';
+//services
+import suppliesAdapterService from '../../services/supplies-adapter.service';
+import {
+  SelectorCheckIfElementExistsByUNID,
+  SelectorGetCurrentSupplie,
+} from '../../store/slectors/selectors';
+//types
+import type { ISuppliesItem } from '../../types';
+import type { IItemDataToDisplay } from '../../../../types';
+
+const CountableItemInfo = (): JSX.Element => {
+  const { UNID } = useParams();
+
+  const itemFromUnidIsset = useAppSelector(SelectorCheckIfElementExistsByUNID(UNID));
+  const item: ISuppliesItem | undefined =
+    UNID && itemFromUnidIsset ? useAppSelector(SelectorGetCurrentSupplie(UNID)) : undefined;
+
+  const dispatch = useAppDispatch();
+
+  let convertedItem: IItemDataToDisplay | undefined;
+
+  if (item !== undefined && item) {
+    convertedItem = suppliesAdapterService.adaptItemDataToDisplay(item);
+  } else {
+    convertedItem = undefined;
+  }
+
+  const clearDataHandler = () => {
+    if (item) {
+      dispatch(clearItemDataAction({ item: item }));
+    }
+  };
+
+  return <ShortCardWithLogsModal item={convertedItem} clearDataHandler={clearDataHandler} />;
+};
+
+export default CountableItemInfo;
--- src/app/supplies/pages/supplies-counter-page/supplies-counter-page.tsx
+++ src/app/supplies/pages/supplies-counter-page/supplies-counter-page.tsx
@@ -1,5 +1,6 @@
 //layouts
 import { CounterPageLayout } from '../../../../layouts';
+import { CountableItemInfo } from '../../components';
 //components
 //variables
 import { SuppliesAppRouting } from '../../variables';
@@ -9,7 +10,9 @@ import style from './supplies-counter-page.module.scss';
 const SuppliesCounterPage = (): JSX.Element => {
   return (
     <CounterPageLayout backLink={SuppliesAppRouting.ROOT.path} headerTitle={'licznik komponentów'}>
-      <main className={style['counter-page']}></main>
+      <main className={style['counter-page']}>
+        <CountableItemInfo />
+      </main>
     </CounterPageLayout>
   );
 };
