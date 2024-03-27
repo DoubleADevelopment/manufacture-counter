217013edc66bef049d1700dc2783c3ced3f8ef68
Anton Oshurek <oshurekdev@proton.me>
2024-03-27 18:20:39 +0100

    replace chemistry root page to component root page

--- src/app/chemistry/router/router.tsx
+++ src/app/chemistry/router/router.tsx
@@ -1,13 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
+//state
+import { SelectorGetChemistry } from '../store/slectors/selectors';
 //variables
-import { ChemistryAppRouting } from '../variables';
+import { CHEMISTRY_TEXT, ChemistryAppRouting } from '../variables';
 //pages
 import * as Pages from '../pages';
+import { ComponentRootPage } from '../../../pages';
 
 const Router = (): JSX.Element => {
   return (
     <Routes>
-      <Route index element={<Pages.ChemistryRootPage />} />
+      <Route
+        index
+        element={
+          <ComponentRootPage
+            SelectorGetComponentState={SelectorGetChemistry}
+            headerTitle={CHEMISTRY_TEXT.CHEMISTRY}
+          />
+        }
+      />
       <Route path={ChemistryAppRouting.COUNTER.route} element={<Pages.CounterPage />} />
     </Routes>
   );
