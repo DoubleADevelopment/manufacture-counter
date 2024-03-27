4648a36db62c0bce9c0b447dab49ac450be3cee1
Anton Oshurek <oshurekdev@proton.me>
2024-03-27 18:18:17 +0100

    add component root page

---
+++ src/pages/component-root-page/component-root-page.tsx
@@ -0,0 +1,45 @@
+//store
+import { useAppSelector } from '../../hooks/hooks';
+//services
+import { adapterService } from '../../services';
+//components
+import { ItemsList, PageNotification } from '../../components';
+//layouts
+import { PageWithMenuLayout } from '../../layouts';
+//variables
+import { AppRouting, ErrorsText, NotificationType } from '../../variables';
+//types
+import type { IData, RootState } from '../../types';
+//styles
+import style from './component-root-page.module.scss';
+
+interface IComponentRootPageProps {
+  headerTitle: string;
+  SelectorGetComponentState: () => (state: RootState) => IData;
+}
+
+const ComponentRootPage = ({
+  SelectorGetComponentState,
+  headerTitle,
+}: IComponentRootPageProps): JSX.Element => {
+  const componentState = useAppSelector(SelectorGetComponentState());
+  const adaptedData = adapterService.adaptItemsDataToDisplayList(componentState);
+
+  return (
+    <PageWithMenuLayout headerTitle={headerTitle} backLink={AppRouting.ROOT.path}>
+      <main className={style['component-root-page']}>
+        {adaptedData ? (
+          <ItemsList data={adaptedData} />
+        ) : (
+          <PageNotification
+            type={NotificationType.ERROR}
+            headingText={ErrorsText.SOMETHING_WENT_WRONG}
+            paragraphText={ErrorsText.ITEMS_LIST_NOT_FOUND}
+          />
+        )}
+      </main>
+    </PageWithMenuLayout>
+  );
+};
+
+export default ComponentRootPage;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import InfoPage from './info-page/info-page';
 import MainPage from './main-page/main-page';
 import ComponentOverviewPage from './components-overview-page/components-overview-page';
 import SettingsPage from './settings-page/settings-page';
+import ComponentRootPage from './component-root-page/component-root-page';
 
-export { MainPage, InfoPage, ComponentOverviewPage, SettingsPage };
+export { MainPage, InfoPage, ComponentOverviewPage, SettingsPage, ComponentRootPage };
